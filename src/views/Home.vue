d5b801da488db4dd8ca5804a89bc0e91bc7c870c
chanwaichun <chanwaicuncc@163.com>
2019-05-28 16:24:00 +0800

    first

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <p>HelloWorld</p>
+    <p>{{this.$store.state.username}}</p>
+    <HelloWorld :msg="message" />
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'home',
+  data()
+  {
+    return({
+       message:"helloworld"
+    })
+   
+  },
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+<style lang="scss" scoped>
+.home
+{
+  $color:red;
+  p{
+    color:$color;
+  }
+}
+</style>
---
+++ src/views/Test.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="test">
+        <div class="nav clearfix">
+            <ul>
+                <li v-for="(item,index) in list" :key="index" @mouseover="show($event,index)" @mouseleave="hide($event,index)">{{item}}</li>
+            </ul>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+     name:'test',
+     data()
+     {
+         return({
+             list:[1,2,3,4],
+             currentIndex:null,
+         })
+     },
+     methods:
+     {
+         show(event,index)
+         {
+
+             this.currentIndex = index;
+          
+             event.target.style.transition="all 1s";
+             event.target.style.backgroundColor="red";
+             event.target.style.height="100px";
+
+         },
+         hide(event,index)
+         {
+             this.currentIndex = index;
+             event.target.style.transition="all 1s";
+             event.target.style.backgroundColor="white";
+             event.target.style.height="40px";
+         }
+        
+     }   
+}
+</script>
+<style lang="scss" scoped>
+    .test
+    {
+        width: 100%;
+
+        .nav
+        {
+            width:1000px;
+            margin:0 auto;
+            ul
+            {
+                float:right;
+                li
+                {
+                    text-align: center;
+                    height: 40px;
+                    width: 100px;
+                    line-height: 40px;
+                    float:left;
+                    // list-style: none;
+                }
+            }
+        }
+    }
+</style>
